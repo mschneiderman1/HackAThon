0b7b6a4b4df140dd64856ce584da84515c998861
mschneidz <mss.tarheel2@gmil.com>
2019-04-03 13:17:10 -0400

    First commit with base project

---
+++ src/ArrayFunction.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ArrayFunction extends JApplet {
+    public void init(){
+
+    }
+    public void paint(Graphics g){
+        g.drawOval(0,0,250,100);
+        g.setColor(Color.RED);
+        g.drawString("First Applet",10, 50);
+
+    }
+}
