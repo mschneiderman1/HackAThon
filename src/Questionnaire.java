a79ec1935ba7c735be756b9f13e4790ef61cd3f5
mschneidz <mss.tarheel2@gmil.com>
2019-04-06 10:56:12 -0400

    Removed part with logos

--- src/Questionnaire.java
+++ src/Questionnaire.java
@@ -1,19 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Questionnaire {
-    public static void main(String []args){
-        intro();
-        //System.out.println(q1());
-    }
-    public static void intro(){
-        System.out.println("Please Answer the following questions: \n\n");
-    }
-    public static double q1(){
-        Scanner console = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner input = new Scanner(new File("answers.txt"));
+        PrintStream output = new PrintStream(new File("OutputAnswers.txt"));
+        double[] waterGallons = {};
+        createArray(input);
 
-        System.out.println("What is the average length of your showers in minutes?");
-        int hours = console.nextInt();
+    }
+    public static double[] createArray(Scanner input){
+        double[] array  = new double[10];
+        double i = 0;
+        while(input.hasNext()){
+            array[i] = input.nextInt();
+        }
+        return array;
+    }
 
-        return hours * 8.2;
+    public static void printOutput(PrintStream output, double[] array, double[] waterGallons){
+        double total = 0.0;
+        for(int i = 0; i < array.length; i++){
+            total += waterGallons[i] * array[i];
+            output.println(waterGallons[i] * array[i]);
+        }
+
     }
 }
