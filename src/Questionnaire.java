78ced793f956a61c2aa2df8fed4d7d7bc4b5cd4a
mschneidz <mss.tarheel2@gmil.com>
2019-04-06 11:03:41 -0400

    Removed part with logos

--- src/Questionnaire.java
+++ src/Questionnaire.java
@@ -7,12 +7,13 @@ public class Questionnaire {
     public static void main(String[] args) throws FileNotFoundException{
         Scanner input = new Scanner(new File("answers.txt"));
         PrintStream output = new PrintStream(new File("OutputAnswers.txt"));
-        double[] waterGallons = {};
-        createArray(input);
+        double[] waterGallons = {2.1, 1800, 30, 659, 3, 16.5, 53};
+        double[] array = createArray(input);
+        printOutput(output, array, waterGallons);
 
     }
     public static double[] createArray(Scanner input){
-        double[] array  = new double[10];
+        double[] array  = new double[7];
         int i = 0;
         while(input.hasNext()){
             array[i] = input.nextInt();
